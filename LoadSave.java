81ec8365bb0ed4da45274db1d905425372581f84
Jack D'Amelio <jadamelio@gmail.com>
2016-07-29 12:05:42 -0700

    no message

--- LoadSave.java
+++ LoadSave.java
@@ -7,32 +7,33 @@ public class LoadSave {
 	
 	public ArrayList<int[][]> load(String a){
 		ArrayList<int[][]> toLoad = new ArrayList<int[][]>();
-			try{
-				Scanner openFile = null;
-				openFile = new Scanner(new File(a));
-				int dim = openFile.nextInt();
-				int[][] blank = new int[dim][dim];
-				while(openFile.hasNextLine()){
-					int x;
-					int y;
-					int next;
-					next = openFile.nextInt();
-					if(!(next == -1)){
-						x = next;
-						y = openFile.nextInt();
-						blank[x][y] = 1;
-					}
-					else{
-						toLoad.add(blank);
-						blank = new int[dim][dim];
-					}
+		try{
+			Scanner openFile = null;
+			openFile = new Scanner(new File(a));
+			int dim;
+			dim = openFile.nextInt();
+			int[][] blank = new int[dim][dim];
+			while(openFile.hasNextLine()){
+				int x;
+				int y;
+				int next;
+				next = openFile.nextInt();
+				if(!(next == -1)){
+					x = next;
+					y = openFile.nextInt();
+					blank[x][y] = 1;
 				}
-				openFile.close();
+				else{
+					toLoad.add(blank);
+					blank = new int[dim][dim];
+				}
 			}
-			catch(Exception e){
-				System.out.println("Opening didn't work");
-			}
-			return toLoad;
+			openFile.close();
+		}
+		catch(Exception e){
+			System.out.println("Loading didn't work");
+		}
+		return toLoad;
 	}
 	
 	public void save(ArrayList<int[][]> toSave, String a){
--- Simulator.java
+++ Simulator.java
@@ -1,4 +1,3 @@
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -7,7 +6,7 @@ import java.util.Stack;
 
 public class Simulator {
 
-	private int[][] rootMap;
+	private int[][] rootMap = new int[9999][9999];
 	private ArrayList<int[][]> mapProgress = new ArrayList<int[][]>();
 	private Stack<Order> stack = new Stack<Order>();
 
@@ -21,8 +20,14 @@ public class Simulator {
 
 	}
 
+	public Simulator(int[][] map) {
+		ArrayList<int[][]> a = new ArrayList<int[][]>();
+		rootMap = map;
+		a.add(map);
+	}
+
 	public Simulator() {
-		rootMap = new int[100][100];
+		rootMap = new int[20][20];
 		rootMap[8][8] = 1;
 		rootMap[8][9] = 1;
 		rootMap[8][10] = 1;
@@ -33,105 +38,138 @@ public class Simulator {
 		rootMap[9][11] = 1;
 	}
 
-	public ArrayList<int[][]> run() throws FileNotFoundException, UnsupportedEncodingException {
-		
+	public ArrayList<int[][]> run() {
+
 		int numChanges = 1;
 		mapProgress.add(rootMap.clone());
 		int counter = 1;
 		int[][] blank;
 		while (numChanges != 0) {
-			//if(counter > 1000)break;
+			// if(counter > 1000)break;
 			numChanges = 0;
 			blank = new int[rootMap.length][rootMap.length];
 			for (int i = 0; i < rootMap.length; i++) {
 				for (int j = 0; j < rootMap[i].length; j++) {
 
 					if (mapProgress.get(counter - 1)[i][j] == 0
-							&& getAdjacent(mapProgress.get(counter - 1), new CoordPair(i, j)).length == 3) {
+							&& getAdjacent(mapProgress.get(counter - 1),
+									new CoordPair(i, j)).length == 3) {
 						stack.push(new Order(new CoordPair(i, j), 1));
 						numChanges++;
 					}
 
 					if (mapProgress.get(counter - 1)[i][j] == 1
-							&& getAdjacent(mapProgress.get(counter - 1), new CoordPair(i, j)).length > 3) {
+							&& getAdjacent(mapProgress.get(counter - 1),
+									new CoordPair(i, j)).length > 3) {
 						stack.push(new Order(new CoordPair(i, j), 0));
 						numChanges++;
 					}
 
 					if (mapProgress.get(counter - 1)[i][j] == 1
-							&& getAdjacent(mapProgress.get(counter - 1), new CoordPair(i, j)).length == 2 ||
-							getAdjacent(mapProgress.get(counter - 1), new CoordPair(i, j)).length == 3){
+							&& getAdjacent(mapProgress.get(counter - 1),
+									new CoordPair(i, j)).length == 2
+							|| getAdjacent(mapProgress.get(counter - 1),
+									new CoordPair(i, j)).length == 3) {
 						stack.push(new Order(new CoordPair(i, j), 1));
 						System.out.println("dang");
 						numChanges++;
 					}
 
 					if (mapProgress.get(counter - 1)[i][j] == 1
-							&& getAdjacent(mapProgress.get(counter - 1), new CoordPair(i, j)).length < 2) {
+							&& getAdjacent(mapProgress.get(counter - 1),
+									new CoordPair(i, j)).length < 2) {
 						stack.push(new Order(new CoordPair(i, j), 0));
 						numChanges++;
 					}
 
 				}
+
 			}
 			Order current;
 			while (!stack.isEmpty()) {
 				current = stack.pop();
-				blank[current.getLoc().getxCoord()][current.getLoc().getyCoord()] = current.getSetValue();
+				blank[current.getLoc().getxCoord()][current.getLoc()
+						.getyCoord()] = current.getSetValue();
 			}
-			if(mapProgress.size() > 1){
-			int change = 0;
-			for(int i = 0; i < mapProgress.get(0).length; i++){
-		        for(int j = 0; j < mapProgress.get(0)[i].length ;j++){
-		          
-		          if(mapProgress.get(0)[i][j] != mapProgress.get(mapProgress.size()-1)[i][j]){
-		           change = 1;
-		           break;
-		          }
-		        }
-		        if(change != 0){
-		        	break;
-		        }
-		        
-			}
-		if(change == 0){
-       	 System.out.println("Ini: Period: " + (mapProgress.size() - 1) );
-	            return mapProgress;
-		}
-		}
-			if(mapProgress.size() > 1){
-				int change = 0;
-				for(int i = 0; i < mapProgress.get(0).length; i++){
-			        for(int j = 0; j < mapProgress.get(0)[i].length ;j++){
-			          
-			          if(mapProgress.get(mapProgress.size() - 2)[i][j] != mapProgress.get(mapProgress.size()-1)[i][j]){
-			           change = 1;
-			           break;
-			          }
-			        }
-			        if(change != 0){
-			        	break;
-			        }
-			        
-				}
-			if(change == 0){
-	       	 System.out.println("stagnant: Period: " + (mapProgress.size() - 1) );
-		            return mapProgress;
-			}
-			}
-			mapProgress.add(blank);
-			counter++;
 
+			
+			System.out.println("1");
+			// Checks for a perfect loop from initial conditions
+			if (mapProgress.size() > 1) {
+				System.out.println("2");
+				if (isSame(0, mapProgress.size() - 1)) {
+					System.out.println("3");
+					System.out.println("Ini: Period: "
+							+ (mapProgress.size() - 1));
+					return mapProgress;
+				}
+
+				// Checks for a loop of non dynamic nature
+				if (mapProgress.size() > 2) {
+
+					if (isSame(mapProgress.size() - 1, mapProgress.size() - 2)) {
+						System.out.println("stagnant: Period: "
+								+ (mapProgress.size() - 1));
+						return mapProgress;
+					}
+				}
+
+				// Checks for loop forming after an initial state
+				if (mapProgress.size() > 1) {
+					for (int i = mapProgress.size() - 1; i > 0; i--) {
+						for (int j = i - 1; j > 0; j--) {
+							if (isSame(i, j)) {
+								System.out.println("Ini: Formed Period: "
+										+ (i - j));
+								return mapProgress;
+							}
+						}
+					}
+				}
+			}
+				mapProgress.add(blank);
+				counter++;
+
+			
 		}
 		return mapProgress;
 
 	}
 
-	public void saveMap() throws FileNotFoundException, UnsupportedEncodingException{
-		LoadSave a = new LoadSave();
-		a.save(mapProgress, "SavedRun.txt");
+	public boolean isSame(int a, int b) {
+		for (int i = 0; i < mapProgress.get(0).length; i++) {
+			for (int j = 0; j < mapProgress.get(0)[i].length; j++) {
+				if (mapProgress.get(a)[i][j] != mapProgress.get(b)[i][j]) {
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 	
+
+
+
+	public void saveMap() throws FileNotFoundException,
+			UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("SavedRun.txt", "UTF-8");
+		writer.println(mapProgress.get(0).length);
+		for (int i = 0; i < mapProgress.size(); i++) {
+			for (int j = 0; j < mapProgress.get(i).length; j++) {
+				for (int k = 0; k < mapProgress.get(i).length; k++) {
+					if (mapProgress.get(i)[j][k] == 1) {
+						writer.println(j);
+						writer.println(k);
+					}
+				}
+			}
+			writer.println("-1");
+		}
+		writer.println("finished");
+		writer.close();
+	}
+
 	public CoordPair[] getAdjacent(int[][] map, CoordPair a) {
 
 		ArrayList<CoordPair> adjac = new ArrayList<CoordPair>();
@@ -161,7 +199,8 @@ public class Simulator {
 		}
 		//
 
-		if (a.getxCoord() + 1 < rootMap.length && a.getyCoord() + 1 < rootMap.length) {
+		if (a.getxCoord() + 1 < rootMap.length
+				&& a.getyCoord() + 1 < rootMap.length) {
 			if (map[a.getxCoord() + 1][a.getyCoord() + 1] == 1) {
 				adjac.add(new CoordPair(a.getxCoord() + 1, a.getyCoord() + 1));
 			}
